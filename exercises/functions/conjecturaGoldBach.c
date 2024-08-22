4c8e3bc07b874c96cde4185abb1df0eefe47dd79
GustavoXavier <157379254+Xustavo@users.noreply.github.com>
2024-08-22 12:28:55 +0000

    feat: finishing conjecturaGoldBach.c

--- exercises/functions/conjecturaGoldBach.c
+++ exercises/functions/conjecturaGoldBach.c
@@ -2,31 +2,30 @@
 
 #include <stdio.h>
 
-void primo(x);
+int primo(int x);
 
 int main() {
 
-    int par;
+    int par, i, retorno1, retorno2;
     scanf("%d", &par);
 
-    primo(par);
+    for(i = 1; i <= par; i++){
+        retorno1 = primo(i);
+        retorno2 = primo(par-i);
+        if(retorno1 == 1 && retorno2 == 1){
+            printf("%d e %d\n", i, par - i);
+        }
+
+    }
 
     return 0;
 }
 
-void primo(int x){
-    int primo1, primo2;
-    for(int i = 1; i <= x; i++){
-        for(int divisor = 2; divisor < i; divisor++){
-            if(i%divisor == 0){
-                
-            }
-            primo1 = i;
-            primo2 = x - primo1;
-            if(primo2 %divisor == 0){
-                
-            }
+int primo(int x){
+    for(int divisor = 2; divisor < x; divisor++){
+        if(x%divisor == 0){
+            return 0;
         }
-        printf("%d %d\n", primo1, primo2);
     }
+    return 1;
 }
